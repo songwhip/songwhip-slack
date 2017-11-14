af4361ca99698d03540b71bb9e071406b63ebd49
Wilson Page <wilsonpage@me.com>
2017-11-14 18:09:47 +0000

    Change to not load env vars when in glitch env

--- env.js
+++ env.js
@@ -1,9 +1,15 @@
 const dotenv = require('dotenv')
 const path = require('path')
 
-const { NODE_ENV = 'development' } = process.env
+const {
+  NODE_ENV = 'development',
+  PROJECT_DOMAIN,
+} = process.env
 
-// load the contents of the `.env` file into `process.env`
-dotenv.config({
-  path: path.resolve(__dirname, `./.env.${NODE_ENV}`),
-})
+const IS_GITCH = PROJECT_DOMAIN.includes('gitch')
+
+// gitch handles env vars
+if (!IS_GITCH) {
+  // load the contents of the `.env` file into `process.env`
+  dotenv.config({ path: path.resolve(__dirname, `./.env.${NODE_ENV}`) })
+}
